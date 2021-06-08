7b15ffb6d65427cacfc62801ce755c37df3bad76
jeandieb <johnnydib15@gmail.com>
2021-06-08 01:45:46 -0700

    updated Tenant.py
    updated setters to take user's input and validate it, updated __init__ to use setters to initialize instants of Tenant..

--- Tenant.py
+++ Tenant.py
@@ -1,38 +1,40 @@
+#TODO: update __init__ to use setters to initilize objects,
+#  update setters to validate users' input
+
 class Tenant:
     def __init__(self):
-        self.__first_name = ''
-        self.__last_name = ''
-        self.__phone = ''
-        self.__email = ''
-        self.__SSN = ''
-        self.__apartment = None
+        self.set_first_name()
+        self.set_last_name()
+        self.set_phone()
+        self.set_email()
+        self.set_SSN()
 
-    def set_first_name(self, first_name):
-        self.__first_name = first_name
+    def set_first_name(self):
+        self.__first_name = input('Enter tenant\'s first name: ')
     
     def get_first_name(self):
         return self.__first_name
 
-    def set_last_name(self, last_name):
-        self.__last_name = last_name
+    def set_last_name(self):
+        self.__last_name =  input('Enter tenant\'s last name: ')
 
     def get_last_name(self):
         return self.__last_name
 
-    def set_phone(self, phone):
-        self.__phone = phone
+    def set_phone(self):
+        self.__phone =  input('Enter tenant\'s phone number: ')
     
     def get_phone(self):
         return self.__phone
 
-    def set_email(self, email):
-        self.__email = email
+    def set_email(self):
+        self.__email =  input('Enter tenant\'s email address: ')
 
     def get_email(self):
         return self.__email
 
-    def set_SSN(self, SSN):
-        self.__SSN = SSN
+    def set_SSN(self):
+        self.__SSN =  input('Enter tenant\'s Social Security Number: ')
 
     def get_SSN(self):
         return self.__SSN
@@ -45,3 +47,7 @@ class Tenant:
 
     def print_tenant_apartment_pair(self):
         print(str(self.get_first_name()) + ' ' + str(self.get_last_name()) + '    ' + str(self.__apartment.get_number()))
+
+    def print_tenant(self):
+        return('First name: {} \nLast name: {}\nPhone: {}\nEmail: {}\nSocial Security Number: {}'.format(
+            self.__first_name, self.__last_name, self.__phone, self.__email, self.__SSN))
